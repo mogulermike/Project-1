55b48e318787f5260651e2fa867385c8d6365a66
MD Manning <manningmichaeld@johndeere.com>
2020-09-11 11:40:37 -0500

    Setting up the layout

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Script has been linked!!!")
+
+
+
+//div with fixed dimensions with IDs so i can compare them
+// remove child, append child
+// if im doing with flex, you can prepend instead of append.
